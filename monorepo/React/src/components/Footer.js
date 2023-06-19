1af90a931c04ac6fd79757162820bee7508fe402
qendresakodraliu <qendresakodraliu2@gmail.com>
2023-06-20 01:40:33 +0200

    feat: added pbIcon

--- monorepo/React/src/components/Footer.js
+++ monorepo/React/src/components/Footer.js
@@ -3,7 +3,11 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Powered by Pabau </p>
+      <p>Powered by </p>
+      <p>
+        <img src="./assets/images/pbicon.webp" width={20} height={20}></img>
+      </p>
+      <p>Pabau</p>
     </footer>
   );
 };
