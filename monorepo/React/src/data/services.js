d68f11dba2d72025a6681df0aa2f4aa504fe9baa
qendresakodraliu <qendresakodraliu2@gmail.com>
2023-06-19 17:59:29 +0200

    feat : services

---
+++ monorepo/React/src/data/services.js
@@ -0,0 +1,9 @@
+const services = [
+    {id:'1',name:"Anti Wrinkle Treatment ", image:"https://picsum.photos/50/50?random=1"},
+    {id:'2',name:"Dermal Fillers ", image:"https://picsum.photos/50/50?random=2"},
+    {id:'3',name:"Secret RF ", image:"https://picsum.photos/50/50?random=3"},
+    {id:'4',name:"HArmonyCA  ", image:"https://picsum.photos/50/50?random=4"},
+    {id:'5',name:"Facials ", image:"https://picsum.photos/50/50?random=5"},
+    
+]
+export {services}
--- monorepo/React/src/pages/ServicesPage.js
+++ monorepo/React/src/pages/ServicesPage.js
@@ -1,30 +1,24 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import ServicesCard from "../components/ServicesCard"
+import { services } from "../data/services"
 import "./ServicesPage.css"
+
 
 const ServicesPage =()=>{
     return(
         <div>
             <Header></Header>
-            <div className="ServicesCardContainer" >
+            <div className="services-card-container" >
                 <div className="container" >
-                    <ServicesCard image={"https://picsum.photos/50/50?random=1" } name={"Anti Wrinkle Treatment "} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=2" } name={"Dermal Fillers "} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=3" } name={"Secret RF "} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=4" } name={"HArmonyCA "} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=5" } name={"Profhilo "} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=6" } name={"Facials"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=7" } name={"ScleroTherapy"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=8" } name={"LED"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=9" } name={"Fat Dissolve"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=10" } name={"Growth Factors"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=11" } name={"Special Package for Time Clinic"} />
-                    <ServicesCard image={"https://picsum.photos/50/50?random=12" } name={"Consultation"} />
+                    {services.map(item=>{
+                        return <ServicesCard image={item.image } name={item.name} />
+                    })}
+                    
                 </div>
-                {/* <div>
+                <div className="information-container">
                     <p>Not sure about consultation type? Please, call <a>0203 7959063</a> </p>
-                </div> */}
+                </div>
             </div>
             <Footer></Footer>
 
