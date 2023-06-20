53ccfbc4b806eba3556094bf9a30f8f805e76f80
Qendresa1993 <>
2023-06-20 15:20:56 +0200

    fix: formatted App.js

--- monorepo/React/src/App.js
+++ monorepo/React/src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route,useParams } from 'react-router-dom';
-import ServicesPage from './pages/ServicesPage';
-import SingleServicePage from './pages/SingleServicePage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ServicesPage from "./pages/ServicesPage";
+import SingleServicePage from "./pages/SingleServicePage";
 
 function App() {
-
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<ServicesPage/>} />
-      <Route path="/pages/:id" element={<SingleServicePage />} />
+      <Routes>
+        <Route path="/" element={<ServicesPage />} />
+        <Route path="/pages/:id" element={<SingleServicePage />} />
       </Routes>
-  </Router>
+    </Router>
   );
 }
 
